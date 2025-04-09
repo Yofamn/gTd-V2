89f650f1c4ba8a14a2db1aa3a502c08d36bf32ee
CrazyCase2473 <deacon.mc08@gmail.com>
2025-04-09 14:32:19 -0500

    shopUI code started

--- Assets/Deacons Folder/DeaconScripts/Shop/ShopUI.cs
+++ Assets/Deacons Folder/DeaconScripts/Shop/ShopUI.cs
@@ -5,5 +5,17 @@ using TMPro;
 public class ShopUI : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI AvailableFunds;
-    [SerializeField] TextMeshProUGUI AvailableFundsText;
+    [SerializeField] Transform CategoryUIRoot;
+    [SerializeField] Transform ItemUIRoot;
+
+    [SerializeField] GameObject CategoryUIPrefab;
+    [SerializeField] GameObject ItemUIPrefab;
+
+    public void OnClickedPurchase(){
+
+    }
+    public void OnClickedExit(){
+
+    }
+    
 }
