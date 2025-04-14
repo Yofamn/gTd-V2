d5c89be2197f10e76fdc9e47c3a288452fc10f7f
CrazyCase2473 <deacon.mc08@gmail.com>
2025-04-14 15:03:59 -0500

    Categories showing up on UI

--- Assets/Deacons Folder/DeaconScripts/Shop/ShopUI.cs
+++ Assets/Deacons Folder/DeaconScripts/Shop/ShopUI.cs
@@ -10,7 +10,40 @@ public class ShopUI : MonoBehaviour
 
     [SerializeField] GameObject CategoryUIPrefab;
     [SerializeField] GameObject ItemUIPrefab;
+    [SerializeField] List<ShopItems> AvailableItems;
+    List<ShopItemCategory> ShopCategories;
+    Dictionary<ShopItemCategory, ShopUI_Category> ShopCategoryToUIMap;
 
+    void Start(){
+        RefreshShopUI();
+    }
+    void RefreshShopUI(){
+
+        ShopCategories = new List<ShopItemCategory>();
+        ShopCategoryToUIMap = new Dictionary<ShopItemCategory, ShopUI_Category>();
+
+        //determine category list
+        foreach(var item in AvailableItems){
+            if(!ShopCategories.Contains(item.Category))
+                ShopCategories.Add(item.Category);
+        }
+
+        ShopCategories.Sort((lhs,rhs)=>lhs.Name.CompareTo(rhs.Name));
+
+        //instantiate the categories
+        foreach(var category in ShopCategories){
+
+            var categoryGO = Instantiate(CategoryUIPrefab, CategoryUIRoot);
+            var categoryUI = categoryGO.GetComponent<ShopUI_Category>();
+
+            categoryUI.Bind(category, onCategorySelected);
+            ShopCategoryToUIMap[category] = categoryUI;
+
+        }
+    }
+    void onCategorySelected(ShopItemCategory category){
+
+    }
     public void OnClickedPurchase(){
 
     }
--- Assets/Deacons Folder/DeaconScripts/Shop/ShopUI_Category.cs
+++ Assets/Deacons Folder/DeaconScripts/Shop/ShopUI_Category.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using TMPro;
 public class ShopUI_Category : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI CategoryName;
     ShopItemCategory Category;
 
-    public void Bind(ShopItemCategory category){
+    public void Bind(ShopItemCategory category,UnityAction<ShopItemCategory> onSelectedFn){
         Category = category;
         CategoryName.text = Category.Name;
     }
