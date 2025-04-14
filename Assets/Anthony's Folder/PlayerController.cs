a31ade9ca5929534e8311a61fd54ce284e5e5f89
Yofamn <167719121+Yofamn@users.noreply.github.com>
2025-04-14 15:29:29 -0500

    Turret script V1, or Ai script v2

---
+++ Assets/Anthony's Folder/PlayerController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefence
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float movementSpeed = 10f; //Speed of the player
+
+        const string horizontalAxis = "Horizontal";
+        const string verticalAxis = "Vertical";
+
+        private Rigidbody rb;
+
+        private float horizontalInput => Input.GetAxis(horizontalAxis);
+        private float verticalInput => Input.GetAxis(verticalAxis);
+
+        private void Start()
+        {
+            GetReferences();
+        }
+
+        private void GetReferences()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void FixedUpdate()
+        {
+            Move(); 
+        }
+
+        private void Move()
+        {
+            rb.velocity = new Vector3(horizontalInput, rb.velocity.y, verticalInput)
+                        * movementSpeed;
+        }
+    }
+}
