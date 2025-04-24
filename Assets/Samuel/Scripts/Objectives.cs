32407e04e67318271f6341762a624dc20d98a722
RogueKnight096 <themasterofgames916@gmail.com>
2025-04-24 14:39:37 -0500

    Title to Lobby

---
+++ Assets/Samuel/Scripts/Objectives.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Objective : MonoBehaviour
+{
+    [SerializeField] private Text objectiveDisplay;
+    [SerializeField] private string objectiveText = "I am an objective!!";
+    [SerializeField] private string completedObjective = "Wahoo! You've completed an objective!";
+
+    public UnityEvent OnCompleteObjective = new UnityEvent();
+
+    private void OnEnable()
+    {
+        objectiveDisplay.text = objectiveText;
+    }
+
+    public void CompleteObjective()
+    {
+        if (gameObject.activeSelf)
+        {
+            objectiveDisplay.text = "";
+
+            OnCompleteObjective.Invoke();
+
+            gameObject.SetActive(false);
+        }
+    }
+}
