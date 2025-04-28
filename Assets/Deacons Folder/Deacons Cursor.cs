3007954bb75c623918e6a923eb2b42745aa59e3e
CrazyCase2473 <deacon.mc08@gmail.com>
2025-04-28 15:31:47 -0500

    Error fix

--- Assets/Deacons Folder/Deacons Cursor.cs
+++ Assets/Deacons Folder/Deacons Cursor.cs
@@ -20,7 +20,7 @@ namespace TowerDefense{
         {
             if (Input.GetMouseButtonDown(0))
             {
-                Vector3Int tileCoords = MyGridV2.WorldToGrid(cursor.transform.position);
+                Vector3Int tileCoords = MyGrid.WorldToGrid(cursor.transform.position);
                 TryPlaceTower(grid, tileCoords);
             }
         }
