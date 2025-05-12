0e32669bd1d3d926b7427b66e5949bf64a1b1570
CrazyCase2473 <deacon.mc08@gmail.com>
2025-05-11 22:03:25 -0500

    bug fixes
    disabled second event system
    fixed interaction object script

--- Assets/Madigans Folder/Scripts/Interaction Object.cs
+++ Assets/Madigans Folder/Scripts/Interaction Object.cs
@@ -1,5 +1,5 @@
 
-using System;
+/*using System;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -20,7 +20,7 @@ public class InteractionObject : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return))
+        if(Input.GetKeyDown(KeyCode.E))
         {
             OnInteract.Invoke();
         }
@@ -39,4 +39,82 @@ public class InteractionObject : MonoBehaviour
       OnInteract.Invoke();
    }
 
+} */
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class InteractionObject : MonoBehaviour
+{
+    [SerializeField] private string interactionText = "Press E to interact";  // Text to show when the object is looked at
+    [SerializeField] private string sceneToLoad;  // The scene to load
+    [SerializeField] private float interactionRange = 5f;  // How far the player can interact with the object
+
+    public UnityEvent OnInteract = new UnityEvent();
+
+    private Camera playerCamera;  // The player's camera
+
+    private void Start()
+    {
+        playerCamera = Camera.main;  // Get the player's main camera
+        if (playerCamera == null)
+        {
+            Debug.LogError("Player Camera not found!");
+        }
+
+        OnInteract.AddListener(TestListener);
+        OnInteract.AddListener(LoadScene);  // Add the scene loading listener
+    }
+
+    private void Update()
+    {
+        if (IsLookingAtObject() && Input.GetKeyDown(KeyCode.E))  // Only interact when looking at the object
+        {
+            OnInteract.Invoke();
+        }
+    }
+
+    private bool IsLookingAtObject()
+    {
+        Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);  // Create a ray from the camera to where the player is looking
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactionRange))  // Check if the ray hits something within the interaction range
+        {
+            if (hit.collider.CompareTag("Interactable"))  // Make sure the object has the correct tag (e.g., "Interactable")
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public string GetInteractionText()
+    {
+        return interactionText;
+    }
+
+    void TestListener()
+    {
+        print("Test listener called");
+    }
+
+    public void Interact()
+    {
+        OnInteract.Invoke();
+    }
+
+    void LoadScene()
+    {
+        if (!string.IsNullOrEmpty(sceneToLoad))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+        else
+        {
+            Debug.LogWarning("No scene specified to load!");
+        }
+    }
 }
+
